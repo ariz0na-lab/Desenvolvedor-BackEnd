ce1c9e989de98ec2a6c0bd95c74822ef7be2cfe4
Arizona <andersoncotrinm@gmail.com>
2026-07-17 21:51:06 -0300

    Finalizado aula 1, dia 17/07/2026

--- Lógica de Programação e Algoritmos/SaidaDeDados/SaidaDeDados/Program.cs
+++ Lógica de Programação e Algoritmos/SaidaDeDados/SaidaDeDados/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI"; 
+char caractere = 'A';
+bool logica1 = true;
+bool logica2 = false;
+
+WriteLine(palavra);
+WriteLine($"SENAI Americana {palavra}");
+WriteLine("Curso de DEV BackEND"); //Linha de código para exibir o texto "Curso de DEV BackEND" em cada linha
+Write("Argentina");
+Write(" | Argentina"); //Linha de código para exibir o texto "Argentina" seguido de " | Argentina" na mesma linha
+ReadKey(); //Não exibir intermediário do console
