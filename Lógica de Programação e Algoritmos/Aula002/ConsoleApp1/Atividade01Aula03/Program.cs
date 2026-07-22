e8c4c9dcd79efbf91247a98bc3fcdbc56bc7838b
Arizona <andersoncotrinm@gmail.com>
2026-07-22 19:57:49 -0300

    Aula 03 - Atividade 01

---
+++ Lógica de Programação e Algoritmos/Aula002/ConsoleApp1/Atividade01Aula03/Program.cs
@@ -0,0 +1,23 @@
+﻿//Declaração de variáveis
+int pergunta1;
+int pergunta2;
+int pergunta3;
+
+//Entrada de dados
+Console.WriteLine("1. Insira o valor A");
+pergunta1 = int.Parse(Console.ReadLine());
+Console.WriteLine("\n2. Insira o valor B");
+pergunta2 = int.Parse(Console.ReadLine());
+Console.WriteLine("\n3. Insira o valor C");
+pergunta3 = int.Parse(Console.ReadLine());
+
+
+//Saída de dados
+Console.WriteLine($"\n1. O primeiro valor é igual ao terceiro valor? R:{(pergunta1 == pergunta3 ? "Sim" : "Não")}");
+Console.WriteLine($"\n2. O segundo valor é diferente do segundo valor? R:{(pergunta1 != pergunta2 ? "Sim" : "Não")}");
+Console.WriteLine($"\n3. O segundo valor é maior que o primeiro valor? R:{(pergunta2 > pergunta1 ? "Sim" : "Não")}");
+Console.WriteLine($"\n4. O segundo valor é menor ou igual que o terceiro valor? R:{(pergunta2 <= pergunta3 ? "Sim" : "Não")}");
+
+
+//Não exibir a tela de saída do console automaticamente
+Console.ReadKey();
---
+++ Lógica de Programação e Algoritmos/Aula03/Operadores/Operadores/Program.cs
@@ -0,0 +1,46 @@
+﻿// Operadores relacionais 
+/* <  | menor que
+   >  | maior que
+   <= | menor ou igual a
+   >= | maior ou igual a
+   == | igual a
+   != | diferente de
+*/
+
+/*
+double x = 2.14;
+double y = 2;
+
+Console.WriteLine(x < y);
+Console.WriteLine(x <= y);
+Console.WriteLine(x > y);
+Console.WriteLine(x >= y);
+
+char a = 'S';
+char b = 'N';
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+
+Console.WriteLine(a == b ? "Verdade" : "OK");
+
+//Operadores Lógicos &  | E Lógico
+   || | OU logico condicional
+   !  | Inversão lógica
+    ^ | OU Exclusivo 
+   && | E lógico condicional
+   &  | E lógico, ambos os lados são verdaeiros.
+   |  | OU lógico se um dos valores logicos for verdadeiro, o resultado é verdadeiro.
+
+
+bool x = false;
+bool y = true;
+
+Console.WriteLine(x | y);
+*/
+
+
+
+
+
+
+Console.ReadLine(); //Evitar fechamento automático do console
