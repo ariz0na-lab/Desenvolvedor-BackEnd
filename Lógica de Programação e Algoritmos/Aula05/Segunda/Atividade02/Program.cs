decfdeabe68b77900f1562a040c58e71a8aa23b3
Arizona <andersoncotrinm@gmail.com>
2026-07-27 21:27:56 -0300

    Atividade de calculo finalizada com sucesso

---
+++ Lógica de Programação e Algoritmos/Aula05/Segunda/Atividade02/Program.cs
@@ -0,0 +1,44 @@
+﻿//Atividade 02 - Verificar se os dados foram ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo. Em caso negativo, informe que os dados não formam um triângulo. Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+
+const string JANELA = "ATIVIDADE 02";
+Console.Title = JANELA;
+
+double a, b, c, perimetro, areaTrapezio;
+
+try
+{
+    Console.Write("Digite o valor de A: ");
+    a = double.Parse(Console.ReadLine());
+    Console.Write("Digite o valor de B: ");
+    b = double.Parse(Console.ReadLine());
+    Console.Write("Digite o valor de C: ");
+    c = double.Parse(Console.ReadLine());
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("\nOs valores formam um triângulo.");
+        perimetro = a + b + c;
+        Console.WriteLine($"Perímetro: {perimetro:f1}");
+        Console.ReadKey();
+    }
+    else
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("\nOs valores não formam um triângulo.");
+        areaTrapezio = ((a + b) * c) / 2;
+        Console.WriteLine($"Área do trapézio: {areaTrapezio:f1}");
+        Console.ReadKey();
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+    Console.ReadKey();
+}
+
+double Perimetro()
+{
+    perimetro = a + b;
+    return perimetro;
+}
---
+++ Lógica de Programação e Algoritmos/Aula05/Segunda/Notas/Program.cs
@@ -0,0 +1,71 @@
+﻿//Exercicio NOTAS
+double nota1, nota2, notafinal;
+
+try
+{
+    // Entrada de dados
+    Console.WriteLine("Sistema de Notas.");
+    await Task.Delay(1000);
+    Console.WriteLine("Informe suas notas a seguir para cálculo da nota final.");
+    await Task.Delay(1000);
+    Console.Write("\nDigite a primeira nota: ");
+    nota1 = double.Parse(Console.ReadLine());
+    Console.Write("Digite a segunda nota: ");
+    nota2 = double.Parse(Console.ReadLine());
+    // Processamento de dados
+    Nota();
+    // Saída de dados
+    if (notafinal >= 60) //Atribuição de = ou + de 60
+    {
+        Console.Write("NOTA FINAL: ");
+        await Task.Delay(1000); // Adiciona um atraso de 1 segundo antes de exibir a nota final
+        Console.Write($"{notafinal:f1}");
+        Console.ForegroundColor = ConsoleColor.Green; // Altera a cor do texto para verde
+        Console.Write("\nAPROVADO");
+        Console.ForegroundColor = ConsoleColor.White;   
+        Console.ReadKey(); // Aguarda o usuário pressionar uma tecla antes de encerrar o programa
+    }
+    /* else if (notafinal < 60) //Atribuição de menor que 60
+    {
+        Console.Write("NOTA FINAL: ");
+        await Task.Delay(1000);
+        Console.Write($"{notafinal:f1}");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("\nREPROVADO");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+    */
+    else if (notafinal == 100)
+    {
+        Console.Write("NOTA FINAL: ");
+        await Task.Delay(1000);
+        Console.Write($"{notafinal:f1}");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("\nAPROVADO COM 100% DE APROVEITAMENTO");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+    else //Atribuição de menor que 60
+    {
+        Console.Write("NOTA FINAL: ");
+        await Task.Delay(1000);
+        Console.Write($"{notafinal:f1}");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("\nREPROVADO");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+
+}
+catch (FormatException)
+{
+    Console.WriteLine("Erro: Entrada inválida. Por favor, digite um número válido.");
+    Console.ReadKey();
+}
+
+double Nota()
+{
+    notafinal = nota1 + nota2;
+    return notafinal;
+}
